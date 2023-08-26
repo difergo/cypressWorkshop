2d68e9592319e22bee485c1389c76eaac7b94b75
Diego Fernando Gómez Álvarez <diegofga96@gmail.com>
2023-08-26 14:06:30 -0500

    testing graphql

---
+++ cypress/e2e/graphql.cy.js
@@ -0,0 +1,36 @@
+describe('Test GraphQL', ()=>{
+    it('GraphQL Query', ()=>{
+        const gqlQuery =
+        `query pokemons($limit: Int, $offset: Int) {
+            pokemons(limit: $limit, offset: $offset) {
+              count
+              next
+              previous
+              status
+              message
+              results {
+                url
+                name
+                image
+              }
+            }
+          }`;
+
+        const gqlVariables  = {
+            limit: 20,
+            offset: 0
+        };
+
+        cy.request({
+            method: 'POST',
+            url: 'https://graphql-pokeapi.graphcdn.app/',
+            body: {
+                query: gqlQuery,
+                variables: gqlVariables
+            }
+        }).then(response=>{
+            cy.log(response);
+            expect(response.body.data.pokemons.results[0].name).to.equal('bulbasaur');
+        });
+    });
+});
