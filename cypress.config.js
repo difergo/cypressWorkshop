fb24fc1aa78b64c5429cbb0e779a952265141614
Diego Fernando Gómez Álvarez <diegofga96@gmail.com>
2023-08-28 00:00:52 -0500

    format cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -5,28 +5,28 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/',
     setupNodeEvents(on, config) {
-      on('task', 
-      {
-        queryTestDb: (query)=>{
-          const connection = mysql.createConnection(config.env.db);
-          connection.connect();
+      on('task',
+        {
+          queryTestDb: (query) => {
+            const connection = mysql.createConnection(config.env.db);
+            connection.connect();
 
-          return new Promise((resolve, reject) => {
-            connection.query(query, (err, result) => {
-              if( err ){
-                reject( err );
-              } else {
-                connection.end();
-                return resolve( result );
-              }
+            return new Promise((resolve, reject) => {
+              connection.query(query, (err, result) => {
+                if (err) {
+                  reject(err);
+                } else {
+                  connection.end();
+                  return resolve(result);
+                }
+              });
             });
-          });
-        
+
+          }
         }
-      }
       );
     },
-    excludeSpecPattern:[
+    excludeSpecPattern: [
       "**/1-getting-started/*.js",
       "**/2-advanced-examples/*.js"
     ]
