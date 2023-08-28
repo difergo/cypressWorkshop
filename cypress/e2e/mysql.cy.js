db6f29c4510500bea383eefdb763afad64a9dc9e
Diego Fernando Gómez Álvarez <diegofga96@gmail.com>
2023-08-27 22:54:23 -0500

    more tests with mysql

--- cypress/e2e/mysql.cy.js
+++ cypress/e2e/mysql.cy.js
@@ -1,8 +1,28 @@
 describe('testing database', () => {
 
+    after('Actions after tests', ()=>{
+        cy.log("something like drop the tests db");
+    });
+
+    it('insert',()=>{
+        cy.task('queryTestDb',"INSERT INTO people(id, name) VALUES(11, 'test name 2')").then((response)=>{
+            cy.log(response);
+            expect(response.affectedRows).to.eq(1);
+        })
+
+    });
+
+    it('Delete',()=>{
+        cy.task('queryTestDb',"DELETE FROM people WHERE id=11").then((response)=>{
+            cy.log(response);
+            expect(response.affectedRows).to.eq(1);
+        })
+
+    });
+
     it('select',()=>{
         cy.task('queryTestDb',"select * from people;").then((response)=>{
-            cy.log(response)
+            cy.log(response);
         })
 
     })
